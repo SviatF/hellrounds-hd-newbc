8db8bb116387836b833f455b337dcd0b64993346
SviatF <oleg22777@gmail.com>
2022-07-16 21:37:45 +0300

    hd-bcnews

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,68 @@
 import '../styles/globals.css'
 
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <> 
+  
+  
+  <Component {...pageProps} />
+
+
+    <style jsx global>{`
+   
+   
+
+
+      .bc-row-info   {
+        height:550px;
+        width: 320px;
+        background: #3d3d3d;
+        margin-bottom: 20px;
+    }
+     .bc-row-info p {color: #fff;}
+     .bc-row-info h3{color: #fff;}
+     .bc-row-info .bc-row-ft{color: #fff;}
+    
+    .bc-row-mg img{
+        width: 320px;
+        height: 235px;
+    }
+    .bc-row-txt{
+        color: #000;
+        text-decoration: none;
+        margin: 20px;
+        height: 100%;
+    }
+    .bc-row-txt h3{
+        font-size: 25px;
+        font-weight: 600;
+        text-align: start;
+        height: 10%;
+        overflow: hidden;
+    }
+    .bc-row-txt p{
+        text-align: start;
+        font-size: 15px;
+        margin-top:20px; 
+        height: 35%; 
+        line-height: 1.4;
+        overflow: hidden;
+    }
+    .bc-row-view{
+        float: left;
+        opacity: 0.7;
+    }
+    .bc-row-view p1{
+        margin-left: 5px;
+    }
+    .bc-row-ft span{
+        float: right;
+    }
+    
+    
+    
+    `}</style>
+        </>
 }
 
 export default MyApp
